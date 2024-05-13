b07033a47eaf4858d65f0a0d2e44e0615e6c94ba
Geckopioner <127809174+Geckopioner@users.noreply.github.com>
2024-05-13 12:18:45 +0800

    Add files via upload

---
+++ Peretemuan9/src/peretemuan9/Peretemuan9.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package peretemuan9;
+
+/**
+ *
+ * @author aRTH
+ * 2201010556
+ * 13/05/2024
+ */
+public class Peretemuan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//       new FRM01().setVisible(true);
+        FRM01 fl = new FRM01();
+        fl.setResizable(false);
+        fl.setAlwaysOnTop(true);
+        fl.setVisible(true);
+    }
+    
+}
